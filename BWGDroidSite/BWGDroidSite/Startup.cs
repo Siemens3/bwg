52993ec4bd4b8f391d965b8083253e5de4777d04
Stephen kenny <stephenkenny@outlook.ie>
2017-01-12 14:56:26 +0000

    init
    first init

---
+++ BWGDroidSite/BWGDroidSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BWGDroidSite.Startup))]
+namespace BWGDroidSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
